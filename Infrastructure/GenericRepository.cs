8506fa45c84de4220d61c3b3502a2c605a8d0f2f
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-11 06:04:20 +0200

    Generric Reposistory Added

---
+++ Infrastructure/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Route.Talabat.Core.Entities;
+using Route.Talabat.Core.Repositories.Contract;
+using Route.Talabat.Infrastructure.Data;
+
+namespace Route.Talabat.Infrastructure
+{
+	public class GenericRepository<T> : IGenericRepository<T>  where T : BaseEntity
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public GenericRepository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _dbContext.Set<T>().ToListAsync();
+		}
+
+		public async Task<T?> GetAsync(int id)
+		{
+			return await _dbContext.FindAsync<T>(id);
+		}
+	}
+}
---
+++ Route.Talabat.Core/Repositories.Contract/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Route.Talabat.Core.Entities;
+
+namespace Route.Talabat.Core.Repositories.Contract
+{
+	public interface IGenericRepository<T> where T : BaseEntity
+	{
+		Task<T?> GetAsync(int id);
+
+		Task<IEnumerable<T>> GetAllAsync();
+	}
+}
