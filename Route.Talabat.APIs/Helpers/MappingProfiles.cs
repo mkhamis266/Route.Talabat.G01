112c22526c0e13a2a42dfddf6807ba399563a766
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-16 16:20:56 +0200

    Part 06 Product Picture Url Resolver

--- Route.Talabat.APIs/Helpers/MappingProfiles.cs
+++ Route.Talabat.APIs/Helpers/MappingProfiles.cs
@@ -10,8 +10,8 @@ namespace Route.Talabat.APIs.Helpers
 		{
 			CreateMap<Product, ProductToReturnDTO>()
 				.ForMember(P => P.Brand, O => O.MapFrom(S => S.Brand.Name))
-				.ForMember(P => P.Category, O => O.MapFrom(S => S.Category.Name));
-
+				.ForMember(P => P.Category, O => O.MapFrom(S => S.Category.Name))
+				.ForMember(P => P.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>());
 		}
 	}
 }
