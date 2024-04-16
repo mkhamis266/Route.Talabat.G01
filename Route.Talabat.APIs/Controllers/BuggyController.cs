f506988f3af506bf86773615773697417f475959
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-16 16:50:43 +0200

    Part 08 Error Types at API

---
+++ Route.Talabat.APIs/Controllers/BuggyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Route.Talabat.Infrastructure.Data;
+
+namespace Route.Talabat.APIs.Controllers
+{
+	public class BuggyController : BaseApiController
+	{
+		private readonly ApplicationDbContext _dbcontext;
+
+		public BuggyController(ApplicationDbContext dbcontext)
+        {
+			_dbcontext = dbcontext;
+		}
+
+		[HttpGet("notfound")]
+		public ActionResult GetNotFoundRequest()
+		{
+			var Product = _dbcontext.Products.Find(100);
+			if(Product is not null)
+				return Ok(Product);
+
+			return NotFound();
+		}
+
+		[HttpGet("servererror")]
+		public ActionResult GetServerError()
+		{
+			var Product = _dbcontext.Products.Find(100);
+			var ProductToReturn = Product.ToString();
+			return Ok(ProductToReturn);
+		}
+		[HttpGet("badrequest")]
+		public ActionResult GetBaddRequest()
+		{
+			return BadRequest();
+		}
+		[HttpGet("badrequest/{id}")] //badrequest/five
+		public ActionResult GetBaddRequest(int id)
+		{
+			return Ok();
+		}
+	}
+}
