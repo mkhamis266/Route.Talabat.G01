aa5e64e650a3610ca18498bf4c50d471a2bf8b83
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-29 22:43:52 +0300

    Part 04 Register Endpoint

--- Route.Talabat.APIs/Controllers/AccountController.cs
+++ Route.Talabat.APIs/Controllers/AccountController.cs
@@ -13,8 +13,8 @@ namespace Route.Talabat.APIs.Controllers
 		private readonly UserManager<ApplicationUser> _userManager;
 		private readonly SignInManager<ApplicationUser> _signInManager;
 
-		public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager)
-        {
+		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+		{
 			_userManager = userManager;
 			_signInManager = signInManager;
 		}
@@ -23,17 +23,41 @@ namespace Route.Talabat.APIs.Controllers
 		public async Task<ActionResult<UserDTO>> Login(LoginDTO model)
 		{
 			var user = await _userManager.FindByEmailAsync(model.Email);
-			if (user is null) return Unauthorized(new ApiResponse(401,"Invalid Login"));
+			if (user is null) return Unauthorized(new ApiResponse(401, "Invalid Login"));
 
 			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
-			if(!result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+			if (!result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid Login"));
 
 			return Ok(new UserDTO()
 			{
 				DisplayName = user.DisplayName,
 				Email = model.Email,
 				Token = "will be generated"
-			}) ;
+			});
 		}
-    }
+
+		[HttpPost("register")]
+		public async Task<ActionResult<UserDTO>> Register(RegisterDTO model)
+		{
+			// check if user already exsit
+			var user = new ApplicationUser()
+			{
+				DisplayName = model.DisplayName,
+				Email = model.Email,
+				PhoneNumber = model.PhoneNumber,
+				UserName = model.Email.Split('@')[0],
+			};
+
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (!result.Succeeded) return BadRequest(new ApiValidationErrorResponse() { Errors = result.Errors.Select(E=>E.Description)});
+			
+			return Ok(new UserDTO() 
+			{ 
+				DisplayName = user.DisplayName, 
+				Email = user.Email, 
+				Token = "will be generated" 
+			});
+		}
+	}
 }
---
+++ Route.Talabat.APIs/DTOs/RegisterDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Route.Talabat.APIs.DTOs
+{
+	public class RegisterDTO
+	{
+		[Required]
+		public string DisplayName { get; set; } = null!;
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = null!;
+
+		[Required]
+		[RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z]).{6,}$",ErrorMessage = "password must have 1 uppercase,nmust have 1 lowercase, must have 1 non-alphabetic and at least 6 characters")]
+		public string Password { get; set; } = null!;
+
+		[Required]
+		public string PhoneNumber { get; set; } = null!;
+	}
+}
