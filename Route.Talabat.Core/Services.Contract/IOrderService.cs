53fc435d2b9ce56f82c48111870a70f53c676cef
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-05-04 18:06:02 +0300

    Part 05 Order Service

---
+++ Route.Talabat.Core/Services.Contract/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Route.Talabat.Core.Entities.Order_Aggregate;
+
+namespace Route.Talabat.Core.Services.Contract
+{
+	public interface IOrderService
+	{
+		Task<Order> CreateOrderAsync(string buyerEmail,string basketId,string deliveryMethodId,Address shippingAddress);
+		Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
+		Task<Order> GetOrderByIdAsync(string buyerEmail,int orderId);
+		Task<IReadOnlyList<DelivreyMethod>> GetDelivreyMethodsAsync();
+	}
+}
