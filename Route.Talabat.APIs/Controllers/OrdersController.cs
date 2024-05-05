ff9dbf7dc1cd2ee0e7c280b1ce32650eaf38a64a
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-05-06 02:09:49 +0300

    use [Authorize]

--- Route.Talabat.APIs/Controllers/OrdersController.cs
+++ Route.Talabat.APIs/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System.Security.Claims;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Route.Talabat.APIs.DTOs;
 using Route.Talabat.APIs.Errors;
@@ -22,9 +24,11 @@ namespace Route.Talabat.APIs.Controllers
 		[ProducesResponseType(typeof(OrderToReturnDTO), StatusCodes.Status200OK)]
 		[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
 		[HttpPost]
+		[Authorize]
 		public async Task<ActionResult<OrderToReturnDTO?>> CreateOrder(OrderDTO model)
 		{
 			var address = _mapper.Map<ShippingAddressDTO, ShippingAddress>(model.ShippingAddress);
+
 			var order = await _orderService.CreateOrderAsync(model.BuyerEmail, model.BasketId, model.DeliveryMethodId, address);
 			if (order is null) return BadRequest(new ApiResponse(400));
 			var orderToReturnDto = _mapper.Map<Core.Entities.Order_Aggregate.Order, OrderToReturnDTO>(order);
@@ -32,8 +36,9 @@ namespace Route.Talabat.APIs.Controllers
 		}
 
 		[HttpGet] // GET : /api/Orders?email=""
-		public async Task<ActionResult<IReadOnlyList<OrderToReturnDTO>>> GetOrdersForUser(string email)
+		public async Task<ActionResult<IReadOnlyList<OrderToReturnDTO>>> GetOrdersForUser()
 		{
+			var email = User.FindFirstValue(ClaimTypes.Email) ?? String.Empty;
 			var orders = await _orderService.GetOrdersForUserAsync(email);
 			var orderToReturnDto = _mapper.Map<IReadOnlyList<Core.Entities.Order_Aggregate.Order>, IReadOnlyList<OrderToReturnDTO>>(orders);
 			return Ok(orderToReturnDto);
@@ -42,8 +47,10 @@ namespace Route.Talabat.APIs.Controllers
 		[ProducesResponseType(typeof(OrderToReturnDTO), StatusCodes.Status200OK)]
 		[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
 		[HttpGet("{id}")]
-		public async Task<ActionResult<OrderToReturnDTO>> GetOrderForUser(string email,int id)
+		[Authorize]
+		public async Task<ActionResult<OrderToReturnDTO>> GetOrderForUser(int id)
 		{
+			var email = User.FindFirstValue(ClaimTypes.Email) ?? String.Empty;
 			var order = await _orderService.GetOrderByIdForUserAsync(email, id);
 			if (order is null) return NotFound(new ApiResponse(404));
 			var orderToReturnDto = _mapper.Map<Core.Entities.Order_Aggregate.Order, OrderToReturnDTO>(order);
