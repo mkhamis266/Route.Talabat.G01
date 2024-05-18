f6c003150a98d8e1bc1d139e8b4fb079ef666273
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-05-18 23:13:39 +0300

    Part 06 Test Payment Cycle Without Webhook

--- Route.Talabat.APIs/DTOs/CustomerBasketDTO.cs
+++ Route.Talabat.APIs/DTOs/CustomerBasketDTO.cs
@@ -8,6 +8,7 @@ namespace Route.Talabat.APIs.DTOs
 		public string Id { get; set; }
 
 		public List<BasketItemDTO> Items { get; set; }
+
 		public string? PaymenyIntentId { get; set; }
 		public string? ClientSecret { get; set; }
 		public decimal ShippingPrice { get; set; }
